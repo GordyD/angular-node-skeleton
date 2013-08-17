e3522f7cfde5bbf3aa7a228493c0b97fc737e97b
Gordon Dent <gordonmdent@gmail.com>
2013-08-17 00:29:36 +0000

    add a directive to add google maps autocomplete to a text box - booom

--- public/js/directives.js
+++ public/js/directives.js
@@ -7,4 +7,16 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
+  })
+  .directive('locationLookup', function() {
+  	return function(scope, element, attrs) {
+    	var autocomplete = new google.maps.places.Autocomplete(element[0]);
+
+    	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      		var place = autocomplete.getPlace();
+      		scope.form.destination =place.formatted_address;
+      		console.log(element);
+      		scope.form.geoinfo = { name: place.formatted_address, lat: place.geometry.location.mb, lon: place.geometry.location.nb};
+    	});
+  	}
   });
