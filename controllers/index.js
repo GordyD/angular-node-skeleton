1eb14c8835de2aad90114206f429ddb420d0cfb2
Jonathan Tsai <jontsai@jontsai.com>
2013-09-26 09:45:19 -0700

    clean up some stuff; minor syntax polish

--- controllers/index.js
+++ controllers/index.js
@@ -11,6 +11,7 @@ exports.partials = function (req, res) {
   var name = req.params.name;
   res.render('partials/' + name);
 };
+
 exports.subpartials = function (req, res) {
   var name = req.params.name;
   var directory = req.params.directory;
